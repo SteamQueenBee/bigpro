e380a2cd8682c213c6651d4647c085422b8088c9
SteamQueenBee <reginaldmahendra1@gmail.com>
2019-02-07 15:59:40 +0700

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,7 +14,7 @@ class News extends Component {
                 <p> The anime television series adaptation by Madhouse consists of 3 seasons with 13 episodes each, with the first season airing from July 7 to September 29, 2015. Two compilation anime films recapping the events from the first season were released in Japan on February 25, 2017, and March 11, 2017, respectively. The second season ran from January 9 to April 3, 2018, with the third season premiering later the same year on July 10, 2018, after which the last episode aired on October 2, 2018.</p>
             </Col>
             <Col>
-                <h2>HISTORY</h2>
+                <h3>HISTORY</h3>
                 <p>In February 2004, Madhouse became a subsidiary of Index Corporation.[1]
 
 On February 8, 2011, Nippon TV became Madhouse's primary stockholder (replacing Index Corporation), via a third-party allocation of new shares.[2] NTV bought 128,667 new shares (each ¥7,772) issued by Madhouse for ¥999,999,924 total (about $12.4 million), raising its stake in the company from 10.4% to 84.5%. Index Corporation's stake in Madhouse fell from 60.91% to 10.54%.[3][4]
